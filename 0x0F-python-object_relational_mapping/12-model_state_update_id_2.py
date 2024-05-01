eedc09b06947ccbccba4c04b7441f1b4a479d20e
CaroMusangi1 <kitongacarol8@gmail.com>
2024-05-01 18:03:33 +0000

    tired

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,29 +1,18 @@
-
 #!/usr/bin/python3
-""" changes the name of a state
+""" Write a script that changes the name of a State object from the database
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    host = 'localhost'
-    port = 3306
-
-    db_uri = "mysql://{}:{}@{}:{}/{}".format(username, password, host,
-                                             port, database)
-    engine = create_engine(db_uri, pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).filter(State.id == 2).first()
-    if state:
-        state.name = "New Mexico"
-        session.commit()
-
-    session.close()
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
+    session.commit()
