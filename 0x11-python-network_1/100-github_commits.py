4007bea489349ed515223c4315b8640bad8c5ca0
CaroMusangi1 <kitongacarol8@gmail.com>
2024-05-10 10:33:05 +0000

    rails

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #1/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
 """
-import sys
+script that takes 2 arguments in order to solve this challenge
+"""
+
 import requests
+import sys
 
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
+if __name__ == '__main__':
+    url = f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
+    headers = {'Accept': 'application/vnd.github+json'}
+    r = requests.get(url, headers=headers)
+    comm = r.json()
     try:
         for i in range(10):
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                comm[i].get("sha"),
+                comm[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
